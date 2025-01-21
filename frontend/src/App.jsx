2330766e146d2828a93f968ce4f77f7b996215aa
nuwanlaksitha <nuwanlakshitha649@gmail.com>
2025-01-21 15:01:16 +0530

    hbdih2b
    sjcnsj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from './Components/Navbar';
+import PageLayout from './pages/login';
 
 const App = () => {
   return (
-    <div>
-    <Navbar />
+    <Router>
+      <Navbar/>
+      <Routes>
+        {/* <Route path="/" element={<Home/>}/> */}
+        <Route path='/signin' element={<PageLayout/>}/>
+        {/* <Route path='/login' element={<PageLayout/>}/> */}
+
+
+      </Routes>
+
+    </Router>
   
-    </div>
+    
   )
 }
 
