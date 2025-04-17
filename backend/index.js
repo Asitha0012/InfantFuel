2bbcff43df9e3c01bd311165455f0c44d336c202
Asitha0012 <asithamk62778544@gmail.com>
2025-04-17 21:38:13 +0530

    Backend structures
    backend files for functions

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+
+
+
+//files
+import connectDB from "./config/db.js";
+
+
+
+// Configuration
+dotenv.config();
+connectDB();
+
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 3000;
+
+// routes
+app.use("/api/v1/users", userRoutes);
+
+
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
